40b9af669417a9c1a57312e0669d9451f9ef3974
sali6798 <shaidee.alingcastre@gmail.com>
2020-03-25 22:23:49 -0700

    button responsiveness

--- script.js
+++ script.js
@@ -15,67 +15,8 @@ $(document).ready(function () {
     var numRows = 0;
     var boardType = "";
 
-    //================== BELOW THIS IS BUTTON/GET INPUT VALUE EVENT LISTENERS ==================
-
-    // start game timer
-    function startGame() {
-        // starts timer and displays time
-        var timerInterval = setInterval(function () {
-            timer--;
-            $("#timer").text(timer);
-
-            if ($("#game").hasClass("hide")) {
-                clearInterval(timerInterval);
-            }
-
-            else if (timer === 0 || matchesLeft === 0) {
-                clearInterval(timerInterval);
-                loadEndPage();
-            }
-
-        }, 1000);
-    }
-    
-    function makeCard(boardType, newRow, pos) {
-        var col = $(`<div class='col ${boardType}'>`);
-        var card = $("<div class='card'>");
-        var cardFront = $("<div class='front'>");
-        var cardBack = $("<div class='back card-image'>");
-        console.log(photoArray[pos]);
-        var newImg = $(`<img src="${photoArray[pos]}" alt="card pic">`);
-        cardBack.append(newImg);
-        card.append(cardFront, cardBack);
-        col.append(card);
-        newRow.append(col);
-        $(card).flip({
-            trigger: 'manual'
-        });
-    }
-    
-    function makeGameBoard() {
-        var pos = 0;
-        for (var i = 0; i < numRows; i++) {
-            var newRow = $("<div class='row cardRow'>");
-            for (var j = 0; j < numCols; j++) {
-                makeCard(boardType, newRow, pos);
-                pos++;
-            }
-            $("#cardsContainer").append(newRow);
-        }
-        
-        var secondsLeft = 3;
-        var timerInterval = setInterval(function () {
-            $("#countdown").text(secondsLeft);
-            if (secondsLeft === 0) {
-                clearInterval(timerInterval);
-                $("#overlay").addClass("hide");
-                startGame();
-            }
-            secondsLeft--;
-        }, 1000);
-
-    }
-
+    //===================================================================================
+    // gets the APIs for the topic user chose
     function setGameTopic(numCards) {
         switch (topic) {
             case "dogs":
@@ -87,95 +28,18 @@ $(document).ready(function () {
             default:
                 getLandscape(numCards);
                 break;
-        }      
-    }
-
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * i)
-            const temp = array[i]
-            array[i] = array[j]
-            array[j] = temp
         }
     }
 
-    function getCat(numCards) {
-        catUrl = "https://api.thecatapi.com/v1/images/search?limit=15&apikey=5767aba7-30b0-4677-a169-9bd06be152b8";
-
-        $.ajax({
-            url: catUrl,
-            method: "GET"
-        }).then(function (response) {
-            for (var i = 0; i < numCards / 2; i++) {
-                photoArray.push(response[i].url);
-                photoArray.push(response[i].url);
-            }
-
-            shuffleArray(photoArray);
-            makeGameBoard();
-        });
-    }
-
-    function getPexelsDog(numCards) {
-        var rand = Math.floor(Math.random() * 200 + 1);
-        pexelsUrl = "https://api.pexels.com/v1/search?query=dogs+query&per_page=15&page=" + rand;
-
-        $.ajax({
-            url: pexelsUrl,
-            headers: { "Authorization": "563492ad6f9170000100000189da3e3e71c041369167af3e07e5a355" },
-            method: "GET",
-            type: "text/json"
-        }).then(function (response) {
-            for (var i = 0; i < numCards / 2; i++) {
-                photoArray.push(response.photos[i].src.original);
-                photoArray.push(response.photos[i].src.original);
-            }
-
-            shuffleArray(photoArray);
-            makeGameBoard();
-        });
-
-    }
-   
-    function getLandscape(numCards) {
-        var rand = Math.floor(Math.random() * 200 + 1);
-        pexelsUrl = "https://api.pexels.com/v1/search?query=landscape+query&per_page=15&page=" + rand;
-
-        $.ajax({
-            url: pexelsUrl,
-            headers: { "Authorization": "563492ad6f9170000100000189da3e3e71c041369167af3e07e5a355" },
-            method: "GET",
-            type: "text/json"
-        }).then(function (response) {
-            for (var i = 0; i < numCards / 2; i++) {
-                photoArray.push(response.photos[i].src.original);
-                photoArray.push(response.photos[i].src.original);
-            }
-
-            shuffleArray(photoArray);
-            makeGameBoard();
-        });
-    }
-    
-    // loads end page, ask for user's name
-    function loadEndPage() {
-        $("#game").addClass("hide");
-        $("#back").addClass("hide");
-        $("#end").removeClass("hide");
-
-        if (timer === 0) {
-            $("#endMessage").text("Time is up!");
-        }
-        else {
-            $("#endMessage").text("You won!");
-        }
-        $("#finalScore").text(score);
-
-    }
-
+    // sets all the values needed to 
+    // start up the game
     function setGame() {
+        // user's chosen difficulty
         difficulty = $("#difficulty").val();
+
+        // user's chosen topic
         topic = $("#topic").val();
+
         switch (difficulty) {
             case "moderate":
                 timer = 150;    // 5x4: timer/matchesLeft === 15 seconds per match allowed
@@ -202,6 +66,8 @@ $(document).ready(function () {
         setGameTopic(numCols * numRows);
     }
 
+    // hide everything on the page and display
+    // the nav, game container, score, and timer 
     function loadGamePage() {
         $("#landing").addClass("hide");
         $("#hof").addClass("hide");
@@ -211,9 +77,8 @@ $(document).ready(function () {
         setGame();
         $("#timer").text(timer);
         $("#score").text(score)
-    
     }
-    
+
     // TODO: populate rows in leaderboard if difficulty global var
     // empty, display leaderboard for easy level (default option on view from landing)
     // otherwise display based on difficulty level saved (after user submits name)
@@ -222,6 +87,8 @@ $(document).ready(function () {
         // append to #leaderboardRows
     }
 
+    // reset all values so it's ready
+    // for when the game is played again
     function reset() {
         score = 0;
         timer = 0;
@@ -241,6 +108,7 @@ $(document).ready(function () {
         $('select').formSelect();
     }
 
+    // load landing page coming from game or leaderboard
     function loadLanding() {
         reset();
         $("#leaderboard").addClass("hide");
@@ -248,28 +116,6 @@ $(document).ready(function () {
         $("#hof").removeClass("hide");
         $("#landing").removeClass("hide");
     }
-
-    // event listener for User card selection
-    // $(".card").click(function () {
-    $(document).on("click", ".card", function() {
-
-        
-        // ignore clicks on cards already matched up or in play
-        if ($(this).hasClass("locked") || $(this).hasClass("in-play")) {
-            console.log("card is locked or in-play: " + $(this).find("p").text());
-            return;
-        }
-
-        // show the image 
-        $(this).flip(true);
-        if (isCardOne) {
-            cardOne = $(this);
-            cardOne.addClass("in-play");
-        } else {
-            setTimeout(evaluateMatch, 1000, $(this));
-        }
-        isCardOne = !isCardOne;
-    });
 
     function evaluateMatch(cardTwo) {
         // this is the second card, lets compare the two
@@ -300,18 +146,195 @@ $(document).ready(function () {
         $("#score").text(score);
     }
 
-    // event listeners
+    function checkCardSelection() {
+        // ignore clicks on cards already matched up or in play
+        if ($(this).hasClass("locked") || $(this).hasClass("in-play")) {
+            console.log("card is locked or in-play: " + $(this).find("p").text());
+            return;
+        }
+
+        // show the image 
+        $(this).flip(true);
+        if (isCardOne) {
+            cardOne = $(this);
+            cardOne.addClass("in-play");
+        } else {
+            setTimeout(evaluateMatch, 1000, $(this));
+        }
+        isCardOne = !isCardOne;
+    }
+
+    // loads end page and displays their final score
+    function loadEndPage() {
+        $("#game").addClass("hide");
+        $("#back").addClass("hide");
+        $("#end").removeClass("hide");
+
+        if (timer === 0) {
+            $("#endMessage").text("Time is up!");
+        }
+        else {
+            $("#endMessage").text("You won!");
+        }
+        $("#finalScore").text(score);
+    }
+
+    // start game timer
+    function startGame() {
+        // starts timer and displays the time every second
+        var timerInterval = setInterval(function () {
+            timer--;
+            $("#timer").text(timer);
+
+            // if the user exits midgame back to the landing
+            // clear the timer
+            if ($("#game").hasClass("hide")) {
+                clearInterval(timerInterval);
+            }
+            else if (timer === 0 || matchesLeft === 0) {
+                clearInterval(timerInterval);
+                loadEndPage();
+            }
+        }, 1000);
+    }
+
+    // makes a new card in the grid
+    function makeCard(newRow, pos) {
+        var col = $(`<div class='col ${boardType}'>`);
+        var card = $("<div class='card'>");
+        var cardFront = $("<div class='front'>");
+        var cardBack = $("<div class='back card-image'>");
+        var newImg = $(`<img src="${photoArray[pos]}" alt="card pic">`);
+        cardBack.append(newImg);
+        card.append(cardFront, cardBack);
+        col.append(card);
+        newRow.append(col);
+        $(card).flip({
+            trigger: 'manual'
+        });
+    }
+
+    // display the right amount of rows and colmumns 
+    // filled with cards
+    function makeGameBoard() {
+        // keep position in photoArray
+        var pos = 0;
+
+        // for each row create all the cards, and
+        // then append it to the cardsContainer
+        for (var i = 0; i < numRows; i++) {
+            var newRow = $("<div class='row cardRow'>");
+            for (var j = 0; j < numCols; j++) {
+                makeCard(newRow, pos);
+                pos++;
+            }
+            $("#cardsContainer").append(newRow);
+        }
+
+        // timer for the overlay countdown, starts
+        // as soon as the game board has been loaded
+        var secondsLeft = 3;
+        var timerInterval = setInterval(function () {
+            $("#countdown").text(secondsLeft);
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+                $("#overlay").addClass("hide");
+                startGame();
+            }
+            secondsLeft--;
+        }, 1000);
+    }
+
+    // shuffles the URLs in the array so that the 
+    // matching pictures aren't next to each other
+    function shuffleArray(array) {
+        // goes through each element in the array
+        // and randomly switches it's place
+        // with another element
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * i)
+            const temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+        }
+    }
+
+    //====================================== API CALLS =========================================
+    function getCat(numCards) {
+        catUrl = "https://api.thecatapi.com/v1/images/search?limit=15&apikey=5767aba7-30b0-4677-a169-9bd06be152b8";
+
+        $.ajax({
+            url: catUrl,
+            method: "GET"
+        }).then(function (response) {
+            // since the cards come in pairs, only need to get
+            // pictures for half the number of cards and
+            // push it twice into the array
+            for (var i = 0; i < numCards / 2; i++) {
+                photoArray.push(response[i].url);
+                photoArray.push(response[i].url);
+            }
+
+            shuffleArray(photoArray);
+            makeGameBoard();
+        });
+    }
+
+    function getPexelsDog(numCards) {
+        var rand = Math.floor(Math.random() * 200 + 1);
+        pexelsUrl = "https://api.pexels.com/v1/search?query=dogs+query&per_page=15&page=" + rand;
+
+        $.ajax({
+            url: pexelsUrl,
+            headers: { "Authorization": "563492ad6f9170000100000189da3e3e71c041369167af3e07e5a355" },
+            method: "GET",
+            type: "text/json"
+        }).then(function (response) {
+            for (var i = 0; i < numCards / 2; i++) {
+                photoArray.push(response.photos[i].src.original);
+                photoArray.push(response.photos[i].src.original);
+            }
+
+            shuffleArray(photoArray);
+            makeGameBoard();
+        });
+
+    }
+
+    function getLandscape(numCards) {
+        var rand = Math.floor(Math.random() * 200 + 1);
+        pexelsUrl = "https://api.pexels.com/v1/search?query=landscape+query&per_page=15&page=" + rand;
+
+        $.ajax({
+            url: pexelsUrl,
+            headers: { "Authorization": "563492ad6f9170000100000189da3e3e71c041369167af3e07e5a355" },
+            method: "GET",
+            type: "text/json"
+        }).then(function (response) {
+            for (var i = 0; i < numCards / 2; i++) {
+                photoArray.push(response.photos[i].src.original);
+                photoArray.push(response.photos[i].src.original);
+            }
+
+            shuffleArray(photoArray);
+            makeGameBoard();
+        });
+    }
+
+    //======================= EVENT LISTENERS =========================
+    $(document).on("click", ".card", checkCardSelection);
     $("#start").click(loadGamePage);
     $("#home").click(loadLanding);
     $("#back").click(function () {
         $("#game").addClass("hide");
         loadLanding();
     });
+
     $(".brand-logo").click(function () {
         $("#game").addClass("hide");
         loadLanding();
     })
-    // TODO: add user score to leaderboard
+
     $("#submit").click(function () {
         // gets user input for name
         var user = $("#name").val();
@@ -322,48 +345,11 @@ $(document).ready(function () {
         $("#lbOptionsRow").addClass("hide");
         loadLeaderboard();
     });
+
     $("#hof").click(function () {
         $("#landing").addClass("hide");
         $("#leaderboard").removeClass("hide");
         $("#lbOptionsRow").removeClass("hide");
         loadLeaderboard();
     });
-
-
-
-
-    //================================== BELOW THIS IS API CALLS =========================================
-
-
-    // function getMovie() {
-    //     var theMovie = "star wars";
-
-    //     var queryUrl = "https://www.omdbapi.com/?t=" + theMovie + "&type=movie&apikey=trilogy";
-
-    //     $.ajax({
-    //         url: queryUrl,
-    //         method: "GET"
-    //     }).then(function (response) {
-    //         console.log(response);
-    //         console.log(response.Poster)
-    //     })
-
-    // }
-
-
-    // function getNASA() {
-    //     var randomYear = Math.floor(Math.random() * (2020 - 2015) + 2010);
-    //     var randomMonth = Math.floor(Math.random() * (12 - 1) + 1);
-    //     var randomDate = Math.floor(Math.random() * (28 - 1) + 1);
-    //     var NASAUrl = `https://api.nasa.gov/planetary/apod?date=${randomYear}-${randomMonth}-${randomDate}&api_key=2YVxSEGEXJwH01Wb6QwvfeJyAWtXzKRNBVhIbVJb`
-
-
-    //     $.ajax({
-    //         url: NASAUrl,
-    //         method: "GET"
-    //     }).then(function (response) {
-    //         console.log(response.url);
-    //     })
-    // }
-
 });
